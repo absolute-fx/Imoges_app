7c61d826b1e3de92abe260918e90c37aa72889fa
Fabrice <info@proglab.com>
2017-09-12 14:48:57 +0200

    auto-update

---
+++ app/class/Tools.js
@@ -0,0 +1,19 @@
+class Tools
+{
+    static FileConvertSize (aSize) {
+        aSize = Math.abs(parseInt(aSize, 10));
+        var def = [[1, 'octets'], [1024, 'ko'], [1024*1024, 'Mo'], [1024*1024*1024, 'Go'], [1024*1024*1024*1024, 'To']];
+        for(var i=0; i<def.length; i++){
+            if(aSize<def[i][0])
+                return (aSize/def[i-1][0]).toFixed(2)+' '+def[i-1][1];
+        }
+    }
+
+
+    static sendStatusToWindow(window, type, text) {
+        window.webContents.send(type, text);
+    }
+
+}
+
+module.exports.Tools = Tools;
---
+++ app/class/autoUpdate.js
@@ -0,0 +1,50 @@
+Tools = require("./Tools").Tools;
+
+class autoUpdate
+{
+    constructor(window) {
+        this.window = window;
+        this.autoUpdater =  require("electron-updater").autoUpdater;
+        this.log = require('electron-log');
+        this.isDev = require('electron-is-dev');
+        this.autoUpdater.logger = this.log;
+        this.autoUpdater.logger.transports.file.level = 'info';
+        this.log.info('App starting...');
+
+        this.init();
+    }
+
+    init(){
+        this.autoUpdater.on('checking-for-update', () => {
+            Tools.sendStatusToWindow(this.window, 'message', 'Checking for update...');
+        });
+        this.autoUpdater.on('update-available', (info) => {
+            Tools.sendStatusToWindow(this.window, 'message', 'checking-for-update');
+        });
+        this.autoUpdater.on('update-not-available', (info) => {
+            Tools.sendStatusToWindow(this.window, 'message', 'Update not available.');
+        });
+        this.autoUpdater.on('error', (err) => {
+            Tools.sendStatusToWindow(this.window, 'message', 'Error in auto-updater');
+        });
+        this.autoUpdater.on('download-progress', (progressObj) => {
+            Tools.sendStatusToWindow(this.window, 'message', Math.round(progressObj.percent)+'%');
+            Tools.sendStatusToWindow(this.window, 'download-progress', {
+                'bytesPerSecond': Tools.FileConvertSize(progressObj.bytesPerSecond),
+                'percentValue' : Math.round(progressObj.percent),
+                'percent' : Math.round(progressObj.percent)+'%',
+                'transferred' : Tools.FileConvertSize(progressObj.transferred),
+                'total' : Tools.FileConvertSize(progressObj.total)
+            });
+        });
+        this.autoUpdater.on('update-downloaded', (info) => {
+            setTimeout(function() {
+                this.autoUpdater.quitAndInstall();
+            }, 1000)
+        });
+
+    }
+
+}
+
+module.exports.autoUpdate = autoUpdate;
