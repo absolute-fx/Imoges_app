c73ab4df0172ffa5bced165dd339bf7c047413ad
Fabrice <info@proglab.com>
2017-09-14 22:38:52 +0200

    v0.1.12

--- GruntFile.js
+++ GruntFile.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         jshint: {
             files: ['app/class/*.js', 'app/view/js/page/*.js', 'app/view/js/widgets/*.js', 'app/view/js/menuData.js', 'app/view/js/menuTemplate.js', 'app/view/js/app.js'],
             options: {
@@ -9,13 +10,23 @@ module.exports = function (grunt) {
         clean: {
             contents: ['dist/*'],
         },
+        rename: {
+            main: {
+                files: [
+                    {src: ['dist/Imoges_app Setup *.*'], dest: 'dist/Imoges_app-Setup-<%= pkg.version %>.exe'},
+                ]
+            }
+        },
         "github-release": {
+            "tag_name": "v<%= pkg.version %>",
+            "name": "v<%= pkg.version %>",
+            draft: true,
             options: {
                 repository: 'absolute-fx/Imoges_app', // Path to repository
                 auth: {   // Auth credentials
                     user: 'Proglab',
                     password: 'lapin324468'
-                }
+                },
             },
             files: {
                 src: ['dist/*.*']
@@ -27,6 +38,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.registerTask('before', ['jshint','clean']);
+    grunt.loadNpmTasks('grunt-rename-util');
     grunt.loadNpmTasks('grunt-github-releaser');
-    grunt.registerTask('after', ['github-release']);
+    grunt.registerTask('after', ['rename', 'github-release']);
 };
