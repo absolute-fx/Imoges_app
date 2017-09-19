6db6067b4dd381bca6ef23a8e3597330e1186b82
Manu <info@absolute-fx.com>
2017-09-20 01:52:36 +0200

    Debug boostrap switcher + modifs édition de projet

--- app/view/js/demo/demo.js
+++ app/view/js/demo/demo.js
@@ -1,4 +1,4 @@
-const bootstrapSwitch = require('bootstrap-switch');
+//const bootstrapSwitch = require('bootstrap-switch');
 const iCheck = require('icheck');
 //const {popover, tooltip} = require('bootstrap');
 
@@ -24,12 +24,13 @@ $(function() {
 
     //Custom checkboxes
     //------------------------
+	/*
 		$(".bootstrap-switch").bootstrapSwitch();
 		$('.icheck input').iCheck({
 			checkboxClass: 'icheckbox_minimal-blue',
 			radioClass: 'iradio_minimal-blue'
 		});
-
+	*/
 
 
 	//Demo Background Pattern
